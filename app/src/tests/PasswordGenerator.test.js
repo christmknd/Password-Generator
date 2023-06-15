28296eaf4d563d1fee11623b7ea0e44d09ce4415
christmknd <christmokonda@gmail.com>
2023-06-15 12:22:16 +0200

    TU DONE

--- app/src/tests/PasswordGenerator.test.js
+++ app/src/tests/PasswordGenerator.test.js
@@ -1,13 +1,37 @@
 import { render , fireEvent } from '@testing-library/react';
 import PasswordGenerator from '../components/PasswordGenerator';
 
-test('generate a  password when the button is clicked', () => {
-    const { getByTestId, getByText } = render(<PasswordGenerator />);
-    const submitButton = getByText('Generer');
-    const passwordField = getByTestId('password')
+describe('Password Generator', () => {
+    test('generate a password when the button is clicked', () => {
+        const { getByTestId, getByText } = render(<PasswordGenerator />);
+        const submitButton = getByText('Generer');
+        const passwordField = getByTestId('password')
+    
+        fireEvent.click(submitButton);
+    
+        expect(passwordField).not.toBe('')
+    
+    })
 
-    fireEvent.click(submitButton);
+    test('generate a password with the correct format' , () => {
+        const { getByTestId, getByText } = render(<PasswordGenerator />);
+        const submitButton = getByText('Generer');
+        const passwordField = getByTestId('password')
+    
+        fireEvent.click(submitButton);
 
-    expect(passwordField).not.toBe('')
+        const password = passwordField.textContent;
 
+        // Au moins une minuscule
+        expect(password).toMatch(/[a-z]/);
+
+        // Au moins une majuscules
+        expect(password).toMatch(/[A-Z]/);
+
+        // Au moins un nombre 
+        expect(password).toMatch(/[0-9]/);
+
+        // Au moins un caractère spécial
+        expect(password).toMatch(/[?,.;/:+=\[\]()\{\}\-_@%^£#€]/);
+    })
 })
